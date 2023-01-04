3d6e526b9ba527d4256e6a8be03fbc8916aabbe5
Declan Stockdale Garbutt <53500810+Declan-Stockdale@users.noreply.github.com>
2023-01-04 20:06:17 +1100

    Update 05_Future_work.py

--- pages/05_Future_work.py
+++ pages/05_Future_work.py
@@ -7,5 +7,6 @@ st.title('Future Work')
 '''
 1. Change heatmap to one combined image. This hasn't been performed as the grid coordinates would be difficult to work out without creating a lot of overlap between main holds and aux holds
 2. Fix dependency issues stopping the random forest and LSTM models to load
+3. Fix create new climbs so that when angle changes, grade remains the same
 
 '''
